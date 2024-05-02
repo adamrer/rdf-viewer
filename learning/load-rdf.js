fd32a42dee8ec058ca6c8e5fc7ecfdafe0e80bd4
rerichaa <adam.rericha@gmail.com>
2024-05-02 15:39:36 +0200

    Print only people, that sheldon-cooper knows

--- learning/load-rdf.js
+++ learning/load-rdf.js
@@ -13,20 +13,22 @@ stream.on('data', quad => {
 
 const ns = {
     schema: namespace('http://schema.org/'),
-    rdf: namespace('http://localhost:8080/data/')
+    rdf: namespace('http://localhost:8080/data/person/')
 }
 
 
 // load from HTTP
 const tbbtRes = await fetchRdf('https://zazuko.github.io/tbbt-ld/dist/tbbt.nq')
 const tbbtData = await tbbtRes.dataset()
-
+// people that sheldon-cooper knows
 for (const quad of tbbtData){
     
-    if (quad.predicate.value === ns.schema.knows.value){
+    if (quad.predicate.value === ns.schema.knows.value && quad.subject.value === ns.rdf('sheldon-cooper').value){
         console.log(`${quad.subject.value} ${quad.predicate.value} ${quad.object.value}`)
     }
 }
+
+// load from data.gov
 const datagovRes = await fetch('https://data.gov.cz/sparql?query=define%20sql%3Adescribe-mode%20"CBD"%20%20DESCRIBE%20<https%3A%2F%2Fdata.gov.cz%2Fzdroj%2Fdatové-sady%2F00231151%2F25b6ed9faca088ebbb1064a05a24d010>&output=text%2Fplain')
 let datagovData = await datagovRes.text();
 fs.writeFile("./rdf/datagov.nq", datagovData, function(err) {
