6750f3e8e6de6f7ffb7cd92d69a057b82caad8ab
rerichaa <adam.rericha@gmail.com>
2025-03-16 14:33:20 +0100

    Move html to index

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,46 +1,128 @@
-import '../css/style.css'
-import './loadRdf.ts'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-    <div class="header">
-        <div>
-            <input id="add-data-source-text" placeholder="Enter SPARQL Endpoint URL" type="text" />
-
-            <button id="add-data-source-btn" >Add data source</button>
-            
-            <input type="file" id="source-input" accept=".ttl,.trig,.nt,.n3,.nq" multiple>
+import N3 from "n3";
+import {DataSource, 
+    SparqlDataSource, 
+    FileDataSource, 
+    QuadsRequest, 
+    Fetcher, 
+    FetchedQuads } from './fetchQuads'
 
 
-            <h4>SPARQL endpoint data sources:</h4>
-            <ul id="data-sources">
-            </ul>
-        </div>
+window.onload = function() {
+    addEventListeners();
+    // example data source
+    (document.getElementById('add-data-source-text')! as HTMLInputElement).value = 'https://data.gov.cz/sparql';
+    addDataSource();
+};
+function addEventListeners(): void {
+    document.getElementById('add-data-source-btn')!.onclick = addDataSource; //add sparql data source
+    document.getElementById('fetch-btn')!.onclick = showQuads
+    
+}
 
-        <div>
-            <label for="target-resource">
-                Target source: 
-                <input id="target-resource" type="text" placeholder="Enter Resource URL" value="https://data.gov.cz/zdroj/lokální-katalogy/00064459/1296195519">
-            </label>
-            
-            
-        </div>
+function createQuadsRequest(): QuadsRequest {
+    const endpointUrls = getEndpointUrls()
+    const dataSources: Array<DataSource> = endpointUrls.map(url => new SparqlDataSource(new URL(url)))
+    const dataSourceFiles = getDataSourceFiles()
+    dataSources.push(...dataSourceFiles.map(file => new FileDataSource(file)))
+
+    const entityIri = (document.getElementById('target-resource')! as HTMLInputElement).value
+
+    return {entityIri: entityIri, dataSources: dataSources}
+
+}
+
+
+function addDataSource(): void {
+    const source : HTMLInputElement = document.getElementById('add-data-source-text') as HTMLInputElement;
+    const dataSourcesList  = document.getElementById('data-sources')!;
+
+    const sourceItem = document.createElement("li");
+    sourceItem.innerHTML = `<span>${source.value}</span>`;
+
+    const deleteBtn : HTMLButtonElement = document.createElement('button') as HTMLButtonElement;
+    deleteBtn.textContent = 'Remove';
+    deleteBtn.onclick = function(){
+        deleteBtn!.parentNode!.parentNode!.removeChild(deleteBtn!.parentNode!);
+    };
+    sourceItem.appendChild(deleteBtn);
+    dataSourcesList.appendChild(sourceItem);
+
+    source.value = '';
+
+}
+
+
+function getDataSourceFiles() : Array<File> {
+    const files : FileList = (document.getElementById('source-input') as HTMLInputElement).files!;
+    return Array.from(files);
+
+}
+function getEndpointUrls() : Array<string>{
+    const endpointUrls: Array<string> = [];
+    const dataSourcesElements = document.getElementById('data-sources')!.children;
+    for (let i = 0; i < dataSourcesElements.length; i++){
+        endpointUrls.push(dataSourcesElements[i].children[0].textContent!);
+    }
+    return endpointUrls;
+}
+
+
+async function printQuads(quads : Array<N3.Quad>, endpointUrl : string, fetcher: Fetcher, resultsDiv : HTMLDivElement): Promise<void> {
+    const endpointTitle = document.createElement("h3");
+    endpointTitle.textContent = endpointUrl;
+
+    resultsDiv.appendChild(endpointTitle);
+
+    const list = document.createElement("ul");
+    quads.forEach(async (quad) => {
+        // const subject = resourceUrl;
+        const predicate = quad.predicate.value;
+        const request = createQuadsRequest()
+        request.entityIri = predicate
+        const predicateTitle = await fetcher.getTitle(request)
+
+        const object = quad.object.value;
+        let objectTitle = object
         
-        <div>
-            <label for="display-input">
-                Choose display:
-                <input type="file" id="display-input" accept=".ts,.js">
-            </label>
-        </div>
+        let objectHTML = object
+        if (quad.object.termType !== 'Literal'){
+            request.entityIri = object
+            objectTitle = await fetcher.getTitle(request)
+            objectHTML = `<a href=${object}>${objectTitle}</a>`
+        }
 
-        <div>
-            <button id='fetch-btn' >Fetch Triples</button>
-        </div>
-        
-    </div>
-    <hr>
-    <div id="results">
+        const QuadListItem = `<li><strong>Predicate:</strong> ${predicateTitle} <strong>Object:</strong> ${objectHTML}</li>`;
+        list.innerHTML += QuadListItem;
+    });
+    resultsDiv.appendChild(list);
+}
+async function showQuads(): Promise<void> {
+    const request = createQuadsRequest()
+    const fetcher: Fetcher = new Fetcher()
 
+    const resultsDiv : HTMLDivElement = document.getElementById('results') as HTMLDivElement;
 
-    </div>
-`
+    // Clear previous results
+    resultsDiv.innerHTML = ``;
+    const resultTitle = document.createElement('h2');
+    resultsDiv.appendChild(resultTitle);
+    resultTitle.textContent = `Results for ${await fetcher.getTitle(request)}`
+    
+    // get display
+    const displayFile:File = (document.getElementById('display-input') as HTMLInputElement).files![0]
+    let printQuadsFunction = printQuads;
+    if (displayFile !== undefined){
+        const fileContent = await displayFile.text();
+        const blob = new Blob([fileContent], { type: "application/javascript" });
+        const blobURL = URL.createObjectURL(blob)
+        const displayModule = await import(blobURL)
+        printQuadsFunction = displayModule.printQuads;
+    }
+
+    const quadsBySource: (FetchedQuads|null)[] = await fetcher.fetchQuads(request)
+    quadsBySource.forEach(quads => {
+        printQuadsFunction(quads!.quads, quads!.dataSourceTitle, fetcher, resultsDiv)
+    });
+}
+
 
