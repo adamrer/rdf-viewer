fa45e5802091e90814dc6f1bdf97c1b85864b13b
adamrer <adam.rericha@gmail.com>
2025-06-08 13:05:16 +0200

    Add tests for simple query step builder

---
+++ src/tests/simple-query-step-builder.test.ts
@@ -0,0 +1,146 @@
+import {expect, test} from 'vitest';
+
+import { simpleStepQueryBuilder } from '../simple-query-step-builder';
+import { Select } from '../query';
+
+
+test('creates basic SPOG query', () => {
+    const builder = simpleStepQueryBuilder()
+    const query = builder.graphs().subjects().predicates().objects().build()
+    expect((query as Select).variables.length).toBe(4)
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object ?graph
+WHERE {
+GRAPH ?graph {
+?subject ?predicate ?object .
+}
+}`)
+})
+
+test('creates basic SPO query', () => {
+    const builder = simpleStepQueryBuilder()
+    const query = builder.subjects().predicates().objects().build()
+    expect((query as Select).variables.length).toBe(3)
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object
+WHERE {
+?subject ?predicate ?object .
+}`)
+})
+
+test('creates query with specified predicates', () => {
+    const builder = simpleStepQueryBuilder()
+    const predicates = ['http://purl.org/dc/terms/title', 'http://www.w3.org/2004/02/skos/core#prefLabel']
+    const query = builder.subjects().predicates(predicates).objects().build()
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object
+WHERE {
+VALUES ?predicate { <http://purl.org/dc/terms/title> <http://www.w3.org/2004/02/skos/core#prefLabel> }
+?subject ?predicate ?object .
+}`)
+})
+
+test('creates query with specified objects', () => {
+    const builder = simpleStepQueryBuilder()
+    const objects = [{value: 'Adam'}, 'https://example.com/ns/#adam', {value: '1', languageOrDatatype: 'http://www.w3.org/TR/xmlschema-2/#integer'}, {value: 'číslo', languageOrDatatype: 'cs'}]
+    const query = builder.subjects().predicates().objects(objects).build()
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object
+WHERE {
+VALUES ?object { "Adam" <https://example.com/ns/#adam> "1"^^<http://www.w3.org/TR/xmlschema-2/#integer> "číslo"@cs }
+?subject ?predicate ?object .
+}`)
+})
+
+test('creates query with specified subject', () => {
+    const builder = simpleStepQueryBuilder()
+    const subjectIri = 'https://monitor.statnipokladna.gov.cz/api/opendata/monitor/Priloha-konsolidace/2019_12_Data_CSUIS_PRIL_KONS'
+    const query = builder.subjects([subjectIri]).predicates().objects().build()
+    expect((query as Select).variables.length).toBe(3)
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object
+WHERE {
+VALUES ?subject { <${subjectIri}> }
+?subject ?predicate ?object .
+}`)
+    
+})
+
+test('creates query with specified graph', () => {
+    const builder = simpleStepQueryBuilder()
+    const graphIri = 'http://www.openlinksw.com/schemas/virtrdf#'
+    const query = builder.graphs([graphIri]).subjects().predicates().objects().build()
+    expect((query as Select).variables.length).toBe(4)
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object ?graph
+WHERE {
+VALUES ?graph { <${graphIri}> }
+GRAPH ?graph {
+?subject ?predicate ?object .
+}
+}`)
+})
+
+test('creates query with specified graph, subject, predicate, object', () => {
+    const builder = simpleStepQueryBuilder()
+    const graph = ['http://www.openlinksw.com/schemas/virtrdf#']
+    const subject = ['https://monitor.statnipokladna.gov.cz/api/opendata/monitor/Priloha-konsolidace/2019_12_Data_CSUIS_PRIL_KONS']
+    const predicates = ['http://purl.org/dc/terms/title', 'http://www.w3.org/2004/02/skos/core#prefLabel']
+    const objects = [{value: 'Adam'}, 'https://example.com/ns/#adam', {value: '1', languageOrDatatype: 'http://www.w3.org/TR/xmlschema-2/#integer'}, {value: 'číslo', languageOrDatatype: 'cs'}]
+    const query = builder.graphs(graph).subjects(subject).predicates(predicates).objects(objects).build()
+    expect((query as Select).variables.length).toBe(4)
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object ?graph
+WHERE {
+VALUES ?graph { <${graph[0]}> }
+GRAPH ?graph {
+VALUES ?subject { <${subject[0]}> }
+VALUES ?predicate { ${predicates.map(predicate => `<${predicate}>`).join(' ')} }
+VALUES ?object { "Adam" <https://example.com/ns/#adam> "1"^^<http://www.w3.org/TR/xmlschema-2/#integer> "číslo"@cs }
+?subject ?predicate ?object .
+}
+}`)
+})
+
+test('creates query with limit and offset specified', () => {
+    const builder = simpleStepQueryBuilder()
+    const query = builder.graphs().subjects().predicates().objects().limit(10).offset(9).build()
+    expect((query as Select).variables.length).toBe(4)
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object ?graph
+WHERE {
+GRAPH ?graph {
+?subject ?predicate ?object .
+}
+}
+LIMIT 10
+OFFSET 9`)
+})
+
+test('creates query with language specified', () => {
+    const builder = simpleStepQueryBuilder()
+    const query = builder.graphs().subjects().predicates().objects().lang('cs').build()
+    expect((query as Select).variables.length).toBe(4)
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object ?graph
+WHERE {
+GRAPH ?graph {
+FILTER (isIRI(?object) || isBLANK(?object) || LANG(?object) = "cs")
+?subject ?predicate ?object .
+}
+}`)
+})
+
+test('creates query with everything specified', () => {
+    const builder = simpleStepQueryBuilder()
+    const graph = ['http://www.openlinksw.com/schemas/virtrdf#']
+    const subject = ['https://monitor.statnipokladna.gov.cz/api/opendata/monitor/Priloha-konsolidace/2019_12_Data_CSUIS_PRIL_KONS']
+    const predicates = ['http://purl.org/dc/terms/title', 'http://www.w3.org/2004/02/skos/core#prefLabel']
+    const objects = [{value: 'Adam'}, 'https://example.com/ns/#adam', {value: '1', languageOrDatatype: 'http://www.w3.org/TR/xmlschema-2/#integer'}, {value: 'číslo', languageOrDatatype: 'cs'}]
+    const query = builder.graphs(graph).subjects(subject).predicates(predicates).objects(objects).limit(10).offset(9).lang('cs').build()
+    expect((query as Select).variables.length).toBe(4)
+    expect(query.toSparql()).toBe(`SELECT DISTINCT ?subject ?predicate ?object ?graph
+WHERE {
+VALUES ?graph { <${graph[0]}> }
+GRAPH ?graph {
+VALUES ?subject { <${subject[0]}> }
+VALUES ?predicate { ${predicates.map(predicate => `<${predicate}>`).join(' ')} }
+VALUES ?object { "Adam" <https://example.com/ns/#adam> "1"^^<http://www.w3.org/TR/xmlschema-2/#integer> "číslo"@cs }
+FILTER (isIRI(?object) || isBLANK(?object) || LANG(?object) = "cs")
+?subject ?predicate ?object .
+}
+}
+LIMIT 10
+OFFSET 9`)
+})
