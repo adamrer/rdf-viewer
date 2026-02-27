db218db10a1ff2834fab5af57430e1be45c6d23d
Adam Rericha <adam.rericha@gmail.com>
2026-02-27 16:45:51 +0100

    Don't copy /plugins because it doesn't exist anymore

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 // vite.config.ts
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    // copy plugins to dist/plugins
-    viteStaticCopy({
-      targets: [
-        {
-          src: "plugins/*",
-          dest: "plugins",
-        },
-      ],
-    }),
-  ],
   base: "./",
   build: {
     rollupOptions: {
